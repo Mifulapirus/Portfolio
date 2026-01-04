69600cf1670cbc96384c99d7f4b19f7522ed93a8
Angel <mifulapirus@gmail.com>
2026-01-03 19:36:02 -0600

    cleaned up a bit

--- V2/.next/static/chunks/webpack.js
+++ V2/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9cd62fe86ab03acf"; }
+/******/ 		__webpack_require__.h = function() { return "b83de8f1ac0b078a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
